ddd29ebde336e7e3eb08c5bf449bd2b736654d2f
davidle.codes <davidl.ruizc@gmail.com>
2019-02-05 15:09:50 -0500

    Importing the mocs

--- ecommerce/router/api/products.js
+++ ecommerce/router/api/products.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const productMock = require('../../utils/products/products')
 
 router.get('/', (req, res) => {
     const { query } = req.query
