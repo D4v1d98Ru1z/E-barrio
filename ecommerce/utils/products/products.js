6ac06f48882eae2c562dfedbef14551959a8653f
davidle.codes <davidl.ruizc@gmail.com>
2019-02-05 15:09:43 -0500

    Creating module to contain all the mocks

---
+++ ecommerce/utils/products/products.js
@@ -0,0 +1,14 @@
+const productsMocks = [
+    {
+        name: 'Razer keyboard ',
+        price: 150,
+        img: 'https://d4kkpd69xt9l7.cloudfront.net/sys-master/images/h3d/hb7/9016072011806/razer-huntsman-hero-desktop-gaming-keyboard.jpg'
+    },
+    {
+        name: 'Headset Logitech G433',
+        price: 100,
+        img: 'https://www.logitechg.com/content/dam/gaming/en/products/g433/g433-feature2-desktop.jpg.imgw.1888.1888.jpeg'
+    }
+]
+
+module.exports = productsMocks
