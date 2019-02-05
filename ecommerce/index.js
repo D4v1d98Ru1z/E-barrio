e9f7a336f9a6be9c418430cf4475bb1478731ea7
davidle.codes <davidl.ruizc@gmail.com>
2019-02-05 13:48:57 -0500

    Packages

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const productsRouter = require('./router/products')
+const productsAPIRouter = require('./router/api/products')
 
 app.get('/', (req, res, next) => {
     res.send({
@@ -15,6 +16,7 @@ app.set('views', path.join(__dirname, "views"))
 app.set('view engine', 'pug')
 
 app.use('/products', productsRouter)
+app.use('/api/products', productsAPIRouter)
 
 const server = app.listen(8000, function(){
     console.log(`Server running on port http://localhost:${server.address().port}`)
