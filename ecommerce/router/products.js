76339592c090442f9f228abb1f5d64af54c53eab
davidle.codes <davidl.ruizc@gmail.com>
2019-02-12 17:00:41 -0500

    Fixing some typos

--- ecommerce/router/products.js
+++ ecommerce/router/products.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const ProductService = require('../../services/productServices')
+const ProductService = require('../services/productServices')
 
 const productService = new ProductService()
 
 router.get('/', async function(req, res, next) {
+    const { tags } = req.query
     try {
-        const { tags } = req.query
         const products = await productService.getProducts({ tags })
         // products.pug
         res.render('products', { products })
