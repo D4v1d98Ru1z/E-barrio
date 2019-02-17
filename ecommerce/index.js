a9b285df7c938385775ef613472c5747718912f1
davidle.codes <davidl.ruizc@gmail.com>
2019-02-16 22:32:52 -0500

    Comment index

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,26 +1,33 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const bodyParse = require('body-parser')
 const productsRouter = require('./router/products')
 const productsAPIRouter = require('./router/api/products')
-const bodyParse = require('body-parser')
 
-app.get('/', (req, res, next) => {
-    res.send({
-        hello: 'Hello World'
-    })
-})
+// app
+const app = express()
 
+// middlewares
+app.use(bodyParse.json()) // send req post json body allows to be read
+
+// static files
 app.use('/static', express.static(path.join(__dirname, "public")))
 
+// view engine setup
 app.set('views', path.join(__dirname, "views"))
 app.set('view engine', 'pug')
 
+// routes
 app.use('/products', productsRouter)
 app.use('/api/products', productsAPIRouter)
 
-app.use(bodyParse.json())
 
+// redirect
+app.get('/', (req, res, next) => {
+    res.redirect('/products')
+})
+
+// server
 const server = app.listen(8000, function(){
     console.log(`Server running on port http://localhost:${server.address().port}`)
 })
