226809a8402256d9cfac6ad38bb012610502accf
davidle.codes <davidl.ruizc@gmail.com>
2019-02-19 12:26:37 -0500

    adding some logs for the api

--- ecommerce/router/api/products.js
+++ ecommerce/router/api/products.js
@@ -6,6 +6,8 @@ const productService = new ProductService()
 
 router.get('/', async (req, res, next) => {
     const { tags } = req.query
+
+    console.log('req: ', req.query)
     try {
         const products = await productService.getProducts({ tags })
     
@@ -21,6 +23,8 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:productId', async (req, res, next) => {
     const { productId } = req.params
+
+    console.log('req.params', req.params)
     try {
         const product = await productService.getProduct({ productId })
     
@@ -36,6 +40,8 @@ router.get('/:productId', async (req, res, next) => {
 
 router.post('/', async function(req, res, next) {
     const { body: product } = req;
+
+    console.log('req', req.body)
     try {
         const product = await productService.createProduct({ product })
     
@@ -52,6 +58,8 @@ router.post('/', async function(req, res, next) {
 router.put('/:productId', async (req, res, next) => {
     const { productId } = req.params
     const { body: product } = req
+
+    console.log('req', req.params, req.body)
     try {
         const updateProduct = await productService.updateProduct({ productId, product })
         res.status(200).json({
@@ -67,6 +75,8 @@ router.put('/:productId', async (req, res, next) => {
 router.patch('/:productId', async (req, res, next) => {
     const { productId } = req.params
     const { body: product } = req
+
+    console.log('req', req.params, req.body)
     try {
         const updateProduct = await productService.updateProduct({productId, product})
         res.status(200).json({
@@ -81,6 +91,8 @@ router.patch('/:productId', async (req, res, next) => {
 
 router.delete('/productId', async (req, res, next) => {
     const { productId } = req.params
+
+    console.log('req', req.params)
     try {
         const product = await productService.deleteProduct({ productId })
         res.status(200).json({
