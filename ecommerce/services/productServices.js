fe07164c9f65353501c65e57d166f953cab20ee8
David Ruiz <davidl.ruizc@gmail.com>
2019-03-13 21:30:56 -0500

    Adding mongo query to get products

--- ecommerce/services/productServices.js
+++ ecommerce/services/productServices.js
@@ -1,10 +1,23 @@
 const productMocks = require('../utils/products/products')
+const MongoLib = require('../lib/mongo')
 
 class productService {
-    constructor(){}
+    constructor(){
+        // Define the collection and create a new instance of mongo lib
+        this.collection = 'products'
+        this.mongoDB = new MongoLib()
+    }
 
-    getProducts({ tags }){
-        return Promise.resolve(productMocks)
+    async getProducts({ tags }){
+        // Create a mongo query. To search into an array in mongo: object + $in: tags
+        const query = tags && { tags: {
+            $in: tags
+        }}
+        // Get the products asynchronously 
+        const products = await this.mongoDB.getAll(this.collection, query)
+        
+        // In case that the products never exist return empty array
+        return products || []
     }
     getProduct({ productId }){
         return Promise.resolve(productMocks[0])
